be2ae2be67bdea29813a93f3f14dfcd3ceaeb177
Nicholas Restifo <nsrestifo@hotmail.com>
2017-06-09 21:06:18 -0400

    added ID getters and setters

--- src/main/java/database/BaseTable.java
+++ src/main/java/database/BaseTable.java
@@ -5,4 +5,12 @@ import com.github.jasminb.jsonapi.annotations.Id;
 public class BaseTable {
     @Id
     private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
